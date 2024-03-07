2db9f327caa6fbb58384dbebb93a511d0e70877f
Wendell Moreira Guindani <113864252+Wendell9@users.noreply.github.com>
2024-03-07 12:45:16 -0300

    Adicionando comentários

--- Atividade 29.02 Israel.cpp
+++ Atividade 29.02 Israel.cpp
@@ -12,17 +12,25 @@ using namespace std;
 
 void ImprimeNumero() {
     std::srand(std::time(nullptr));
+    //Define um numero aleatorio inteiro de 0 a 100. No caso o denominador define o limite do valor randomico.
     int a = rand() % 100;
     std::cout << a << endl;
 
+    // Isso cria um gerador de numeros aleatorios com base em alguma fonte de entropia definida pela implementacao 
+    // Ele fornece uma semente para o gerador de numeros aleatorios.
     std::random_device rd;
+    //Isso inicializa o mecanismo de geracao de numeros aleatorios eng com a semente obtida de rd(), que e um valor de semente aleatorio.
     std::default_random_engine eng(rd());
+    //Aqui são definidos os limites dessa geração aleatória. No caso 0 e 100. Além disso o uniform_real_distribution faz com que os numeros que são aleatoriamente
+    //gerados sejam do tipo real
     std::uniform_real_distribution<> distr(0, 100);
 
     std::cout << distr(eng);
 
 }
 
+//O objetivo desse método é receber um vetor desorganizado e organiza-lo por meio do método bubble sort.
+//Após isso ele retorna o vetor organizado.
 std::vector<int> BubbleSort(std::vector<int> arranjo) {
 
     int tamanho = arranjo.size();
@@ -42,15 +50,20 @@ std::vector<int> BubbleSort(std::vector<int> arranjo) {
 void VetorAleatorio() {
     std::srand(std::time(nullptr));
     std::vector<int> vetorInt(100);
+    //Aqui é criado um vetor de 100 posições aleatórias com limites de 0 a 1000. 
+    //Esse vetor é apresentado ao usuario.
+    std::cout<<"Vetor Desorganizados"<<endl;
     for (size_t i = 0; i < 100; i++)
     {
         vetorInt[i] = rand() % 1000;
-    std:cout << vetorInt[i] << endl;
+     std::cout << vetorInt[i] << endl;
     }
 
     std::cout << "Vetor Ordenado" << endl;
+    //Aqui o vetor desorganizado passa pelo método bubble sort e retorna um vetor ordenado que é armazenado em cetorOrganizado.
     std::vector<int> vetorOrganizado = BubbleSort(vetorInt);
-
+     
+    //Abaixo é impresso o vetor organizado.
     for (size_t i = 0; i < 100; i++)
     {
         std::cout<<vetorOrganizado[i]<<endl;
@@ -59,6 +72,6 @@ void VetorAleatorio() {
 
 int main()
 {
-    VetorAleatorio();
+    ImprimeNumero();
 }
 
